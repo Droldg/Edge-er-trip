52456d20f43b1dd789c51eef9301fc334ae04c3e
Droldg <daro23ac@student.cbs.dk>
2025-01-17 23:56:26 +0100

    Nu med logo

--- MesChrome.js
+++ MesChrome.js
@@ -1,17 +1,20 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, session } = require('electron');
+const path = require('path');
 
 function createWindow() {
-  // Opret et nyt BrowserWindow
   const mainWindow = new BrowserWindow({
     width: 1000,
     height: 800,
+    // Tilføj ikon (forudsat at "wavy.png" ligger i "img"-mappen
+    // i samme mappe som denne fil)
+    icon: path.join(__dirname, 'img', 'wavy.png'),
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true
     }
   });
 
-  // Indlæs det messenger
+  // Indlæs Messenger
   mainWindow.loadURL('https://www.messenger.com');
 }
 
