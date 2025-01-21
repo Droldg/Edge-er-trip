a9ec35afed23eeee306c0a1f4c8893099db4ebd7
Droldg <daro23ac@student.cbs.dk>
2025-01-22 00:45:37 +0100

    Update MesChrome.js

--- MesChrome.js
+++ MesChrome.js
@@ -1,12 +1,22 @@
 const { app, BrowserWindow, session } = require('electron');
 const path = require('path');
 
+// Sæt sprogindstilling til dansk (Chromium)
+app.commandLine.appendSwitch('lang', 'da');
+
+// Tving Accept-Language header til "da,en;q=0.8"
+function forceDanishLocale() {
+  session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
+    // Overskriv "Accept-Language"
+    details.requestHeaders['Accept-Language'] = 'da,en;q=0.8';
+    callback({ cancel: false, requestHeaders: details.requestHeaders });
+  });
+}
+
 function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 1000,
     height: 800,
-    // Tilføj ikon (forudsat at "wavy.png" ligger i "img"-mappen
-    // i samme mappe som denne fil)
     icon: path.join(__dirname, 'img', 'wavy.png'),
     webPreferences: {
       nodeIntegration: false,
@@ -18,8 +28,10 @@ function createWindow() {
   mainWindow.loadURL('https://www.messenger.com');
 }
 
-// Når Electron er klart, opret vinduet
+// Når Electron er klart, opretter vi vinduet
 app.whenReady().then(() => {
+  // Inden vi skaber vinduet, sætter vi Accept-Language til dansk
+  forceDanishLocale();
   createWindow();
 });
 
